d3b55018c28669ee84dd5102a14248b5e84ebfb9
Akshay Singh <akshaysingh000@gmail.com>
2022-09-07 15:56:54 -0400

    Add methods to point interface

--- java/src/main/java/edu/cmu/cs/cs214/rec02/points/Point.java
+++ java/src/main/java/edu/cmu/cs/cs214/rec02/points/Point.java
@@ -1,4 +1,15 @@
 package edu.cmu.cs.cs214.rec02.points;
 
 public interface Point {
+    /**
+     * Get x coordinate of the point.
+     * @return Integer representing the X coordinate.
+     */
+    public int getX();
+
+    /**
+     * Get y coordinate of the point.
+     * @return Integer representing the Y coordinate.
+     */
+    public int getY();
 }
